195b583a73617c891ab9852e1e42a53fe81ee51c
jung860420 <jung8604202gmail.com>
2017-12-08 18:09:02 +0900

    adding headline ui

--- src/kr/js/uiHeadLine/_config.js
+++ src/kr/js/uiHeadLine/_config.js
@@ -11,9 +11,17 @@ export default class Config {
 			'/kr/js/uiHeadLine/css/uiHeadLine.css'
 		];
 
-
-		this.option.data = { };
-		this.option.status = { };
+		this.option.data = {
+			circle : [
+				{ title: 'CIRCLE 01', url: '#' },
+				{ title: 'CIRCLE 02', url: '#' },
+				{ title: 'CIRCLE 03', url: '#' },
+				{ title: 'CIRCLE 04', url: '#' },
+				{ title: 'CIRCLE 05', url: '#' },
+				{ title: 'CIRCLE 06', url: '#' }
+			]
+		};
+		this.option.status = {};
 
 		this.option.page = 0;
 		this.option.count = 0;
@@ -21,17 +29,21 @@ export default class Config {
 
 		this.option.selector = {
 			parent: '#uiHeadLine',
+			circle: '#uiHeadLine .circle',
+			circle_film: '#uiHeadLine .circle_film',
+			bg: '#uiHeadLine .bg',
 		};
 
 		this.option.event = {
 			def: 'click.def',
-		}
+			mouse: 'mouseenter.mouse mouseleave.mouse'
+		};
 
 		// 스타일시트 리스트 추가
 		Style.addStyleSheet(this.option.styleSheet);
 
 		// 실행할 메서드 등록
-		this.render([]);
+		this.render([ 'bind', 'append', 'listener' ]);
 
 		let rw = 0, rh = 0, rtime = null, scope = this;
 
@@ -70,6 +82,40 @@ export default class Config {
 				throw err;
 			}
 		});
+	}
+
+	storage (a) {
+
+		a = {};
+
+		let circleStyle = 'style="background-image:url(/kr/js/uiHeadLine/images/circle/{{i}}.jpg);"';
+
+		a.circle = ''+
+			'<div class="circle">'+
+				'<div class="circle_bg"></div>'+
+				'<div class="circle_inner"><div class="circle_mask" '+circleStyle+'></div></div>'+
+			'</div>';
+
+		let bgStyle = 'style="background-image:url(/kr/js/uiHeadLine/images/bg/{{i}}.jpg);"';
+
+		a.bg = '\n\t<div class="bg bg_{{i}}" '+bgStyle+'></div>';
+
+		a.visual = '\n'+
+			'\n<div class="bg_group">{{bg}}\n</div>'+
+			'\n<div class="circle_group">\n<div class="circle_film"></div>\n{{circle}}\n</div>'+
+			'\n<div class="button_down">'+
+				'<a href="#" onclick="return screenDown(this)">'+
+					'<img src="/kr/js/uiHeadLine/images/arr_down.png" alt="콘텐츠 바로가기">'+
+				'</a>'+
+			'</div>'+
+			'\n<div class="ci_gonet"><img src="/kr/js/uiHeadLine/images/gonet.png" alt="gonet"></div>';
+
+		return a;
+	}
+
+	_storage (a) {
+
+		return a;
 	}
 
 	select (a) {
--- src/kr/js/uiHeadLine/_headline.js
+++ src/kr/js/uiHeadLine/_headline.js
@@ -2,6 +2,83 @@ import Config from './_config';
 
 class main extends Config {
 
+	screenDown (arg) {
+
+	}
+
+	listener () {
+
+		const SCOPE = this;
+
+		let Selector = SCOPE.option.selector;
+		let Event = SCOPE.option.event;
+		let Data = SCOPE.option.data;
+
+		let circle = SCOPE.select(Selector.circle);
+		let circle_film = SCOPE.select(Selector.circle_film)[0];
+
+		$DOCUMENT.on(Event.mouse, Selector.circle, function (event) {
+
+			const t = this;
+
+			let index = SCOPE.index(circle, t);
+
+			if (event.type === 'mouseenter') {
+
+				circle_film.style.opacity = '1';
+				circle_film.style.zIndex = '10';
+			}
+
+			if (event.type === 'mouseleave') {
+
+				circle_film.style.opacity = '0';
+				circle_film.style.zIndex = '0';
+			}
+		});
+	}
+
+	bind (arg) {
+
+		const SCOPE = this;
+
+		let Data = SCOPE.option.data;
+
+		let Str = SCOPE.storage();
+		let _Str = SCOPE._storage({ bg: '', circle: '', visual: Str.visual });
+
+		let circles = Data.circle;
+		let circlesLen = Data.circle.length;
+
+		let bg = '', circle = '';
+
+		for (let i=0; i<circlesLen; i++) {
+
+			_Str.bg = Str.bg;
+			_Str.circle = Str.circle;
+
+			bg += '\n'+_Str.bg.replace(SCOPE.reg('i'), i);
+			circle += '\n'+_Str.circle.replace(SCOPE.reg('i'), i);
+		}
+
+		Data.strings = _Str.visual
+			.replace(SCOPE.reg('bg'), bg)
+				.replace(SCOPE.reg('circle'), circle);
+
+		return this;
+	}
+
+	append (arg) {
+
+		const SCOPE = this;
+
+		let Data = SCOPE.option.data;
+		let Selector = SCOPE.option.selector;
+
+		SCOPE.select(Selector.parent)[0].innerHTML = Data.strings;
+
+		return this;
+	}
+
 	resize (callback) {
 
 		const SCOPE = this;

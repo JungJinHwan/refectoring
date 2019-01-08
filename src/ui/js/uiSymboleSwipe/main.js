15a1a589293a0e2c91b01f3ae6a18a5fb920f113
JUNG, JINHWAN <immortalize@irucore.com>
2019-01-08 18:54:01 +0900

    hi

--- src/ui/js/uiSymboleSwipe/main.js
+++ src/ui/js/uiSymboleSwipe/main.js
@@ -7,19 +7,9 @@ function symbole (args) {
 	var Selector = SCOPE.option.selector;
 	var Data = SCOPE.option.data;
 
-	$(Selector.gradientSymbole).on('mouseenter mouseleave dragstart drag dragend', function (event) {
+	$(Selector.gradientSymbole).on('dragstart drag dragend', function (event) {
 
 		var t = $(this);
-
-		// if (event.type == 'mouseenter') {
-
-		// 	t.addClass('in_active');
-		// }
-
-		// if (event.type == 'mouseleave') {
-
-		// 	t.removeClass('in_active');
-		// }
 
 		if ($(Selector.parent).find(':animated').length) {
 
@@ -35,11 +25,14 @@ function symbole (args) {
 
 			event.preventDefault();
 
-			Data.move = (Math.floor(event.pageX) - Data.start) / $(Selector.parent).width() * 100;
+			Data.move = (Math.floor(event.pageX) - Data.start) / document.body.clientWidth * 100;
 
-			Data.range = Data.move - Data.prev;
+			Data.range = Data.move - Data.rangePrev;
 
 			t.css({ 'left': '+=' + Data.range + '%' });
+
+			// 움직이는 만큰 margin 을 계산한 후 심볼의 너비의 대한 % 를 구한다
+			SCOPE.symboleCoreBar( '+=' + (Data.range*document.body.clientWidth/100)*-1 / $(Selector.gradientSymbole).width() * 100 + '%' );
 
 			if(Data.range < 0){
 
@@ -51,17 +44,37 @@ function symbole (args) {
 				Data.touchDir = 'next';
 			}
 
+			Data.rangePrev = Data.move;
 			Data.prev = Data.move;
 
 			SCOPE.createEndFlag();
+			
 		}
 
 		if(event.type == "dragend" ) {
 
-			Data.prev = 0;
+			Data.rangePrev = 0;
 
-			$(Selector.gradientSymbole).stop(0, 1).animate({ 'left': Data.map[Data.idx] + '%' }, 300, 'easeOutCubic');
+			SCOPE.moveEnded();
+			SCOPE.moveMented();
+
+			SCOPE.symboleCoreBar( ((document.body.clientWidth/2) - (Data.map[Data.idx]*document.body.clientWidth/100))/$(Selector.gradientSymbole).width() *100 + '%' );
 		}
+	});
+
+
+	$(Selector.symboleDirBtn).on('click', function (event) {
+
+		event.preventDefault();
+
+		var t = $(this);
+
+		Data.idx = t.data('idx');
+
+		SCOPE.moveEnded();
+		SCOPE.moveMented();
+
+		SCOPE.symboleCoreBar( ((document.body.clientWidth/2) - (Data.map[Data.idx]*document.body.clientWidth/100))/$(Selector.gradientSymbole).width() *100 + '%' );
 	});
 
 	var rtime = 0;
@@ -75,6 +88,7 @@ function symbole (args) {
 			rtime = setTimeout(function () {
 
 				SCOPE.createEndFlag();
+				SCOPE.symboleCoreBar( ((document.body.clientWidth/2) - (Data.map[Data.idx]*document.body.clientWidth/100))/$(Selector.gradientSymbole).width() *100 + '%' );
 
 			}, 100);
 
@@ -84,6 +98,17 @@ function symbole (args) {
 
 	SCOPE.createScreenFlag();
 
+	return 1;
+}
+
+symbole.prototype.symboleCoreBar = function (arg) {
+
+	var Selector = this.option.selector;
+
+	//core 그라데이션 셋
+	$(Selector.symboleCoreBar).css({ 'width': document.body.clientWidth, 'margin-left': arg});
+
+	return 1;
 }
 
 symbole.prototype.createScreenFlag = function () {
@@ -91,15 +116,17 @@ symbole.prototype.createScreenFlag = function () {
 	var Selector = this.option.selector;
 	var Data = this.option.data;
 
-	var pointWidth = $(Selector.parent).width()/Data.screenLen;
+	var pointWidth = document.body.clientWidth/Data.screenLen;
 
 	for (var i=0; i<Data.screenLen; i++) {
 
-		Data.map[i] = Math.floor((pointWidth * i + pointWidth/2) / $(Selector.parent).width() * 100);
+		Data.map[i] = Math.floor((pointWidth * i + pointWidth/2) / document.body.clientWidth * 100);
 	}
 
 	Data.middleLen = Math.ceil( ( Data.screenLen - 1 ) / 2 );
 	Data.idx = Data.middleLen;
+
+	return 1;
 }
 
 symbole.prototype.createEndFlag = function () {
@@ -107,7 +134,7 @@ symbole.prototype.createEndFlag = function () {
 	var Selector = this.option.selector;
 	var Data = this.option.data;
 
-	Data.currPosition = Math.floor( parseInt($(Selector.gradientSymbole).css('left')) / $(Selector.parent).width() * 100 );
+	Data.currPosition = Math.floor( parseInt($(Selector.gradientSymbole).css('left')) / document.body.clientWidth * 100 );
 
 	if (Data.touchDir == 'prev') {
 
@@ -138,11 +165,49 @@ symbole.prototype.createEndFlag = function () {
 	return 1;
 };
 
+symbole.prototype.moveMented = function () {
+
+	var Selector = this.option.selector;
+	var Data = this.option.data;
+
+	var groupWidth = $(sceneGroup).width();
+
+	if (Data.idx == 0) {
+
+		$(sceneGroup).stop(1, 0).animate({ left: groupWidth }, 1000, 'easeOutCubic');
+	}
+
+	if (Data.idx == 1) {
+
+		$(sceneGroup).stop(1, 0).animate({ left: 0 });
+	}
+
+	if (Data.idx == 2) {
+
+		$(sceneGroup).stop(1, 0).animate({ left: groupWidth*-1 }, 1000, 'easeOutCubic');
+	}
+
+	return 1;
+};
+
+symbole.prototype.moveEnded = function () {
+
+	var Selector = this.option.selector;
+	var Data = this.option.data;
+
+	$(Selector.gradientSymbole).stop(0, 1).animate({ 'left': Data.map[Data.idx] + '%' }, 300, 'easeOutCubic');
+
+	return 1;
+}
+
+
 window.Symbole = new symbole ({
 	selector: {
 		parent: "#symbole",
 		gradientSymbole: "#symbole .symbole_core",
-		parentContent: ".slide_parent_content",
+		sceneGroup: "#sceneGroup",
+		symboleDirBtn: '#symbole [data-idx]',
+		symboleCoreBar: '#symbole .symbole_core .core_bar'
 
 	},
 	data: {

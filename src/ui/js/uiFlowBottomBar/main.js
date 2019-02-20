9a94258489e0dff033cf48beb7723a7534854eb7
JUNG, JINHWAN <immortalize@irucore.com>
2019-02-20 17:01:05 +0900

    11

--- src/ui/js/uiFlowBottomBar/main.js
+++ src/ui/js/uiFlowBottomBar/main.js
@@ -1,24 +1,98 @@
-function fBttBar (args) {
+function main (args) {
 
 	var SCOPE = this;
 
 	SCOPE.option = args || {};
 
-	var Selector = SCOPE.option.selector;
-	var Data = SCOPE.option.data;
+	SCOPE.setSection().clickEvent();
 
 	return 1;
 }
 
+main.prototype.setSection = function (arg) {
 
-window.FlowBottomBar = new fBttBar ({
+	var Selector = this.option.selector;
+	var Data = this.option.data;
+
+	var section = $(Selector.section);
+	var sectionLen = section.length;
+
+	var headerHeight =  $(Selector.header).outerHeight(true);
+	var footerHeight =  $(Selector.footer).outerHeight(true);
+
+	for (var i=0; i<sectionLen; i++) {
+
+		Data.map[i] = section.eq(i).outerHeight();
+	}
+	
+	$(Selector.parent).css('height', Data.map[Data.idx]);
+
+	if (document.body.clientHeight - headerHeight > Data.map[Data.idx]) {
+
+		$(Selector.main).css('height', document.body.clientHeight - headerHeight);
+	}
+	else {
+
+		$(Selector.main).css('height', Data.map[Data.idx] + footerHeight);	
+	}
+
+	if (arg) {
+
+		$(Selector.parent).animate({ 'left': ['100%', '0%', '-100%'][Data.idx] }, 1000, 'easeOutExpo');
+	}
+
+	return this;
+};
+
+
+main.prototype.clickEvent = function () {
+ 	
+ 	var SCOPE = this;
+
+	var Selector = this.option.selector;
+	var Data = this.option.data;
+
+	var fBttBar = $(Selector.fBttBar);
+	var fBttBarBtn = $(Selector.fBttBarBtn);
+
+	fBttBarBtn.on('click', function (event) {
+
+		event.preventDefault();
+
+		var t = $(this);
+
+		var idx = t.data('idx');
+
+		Data.idx = idx;
+
+		SCOPE.setSection(1);
+
+	});
+
+	return this;
+};
+
+
+window.Main = new main ({
 	selector: {
-		parent: "#fBttBar",
+		header: '#header',
+		footer: '#footer',
+		main: '#main',
+		parent: "#slideWrap",
+		section: "#slideWrap .slide_section",
+		fBttBar: '#fBttBar',
+		fBttBarBtn: '#fBttBar a'
 
 	},
 	data: {
-		screenLen: 3,
+		idx: 0,
 		map: []
 	}
 
 });
+
+window.onresize = function() {
+
+	Main.setSection();
+	
+};
